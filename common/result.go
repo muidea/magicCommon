2cd6bef2258a3696654a1cb761f6ad4327cc9fd0
muidea <rangh@foxmail.com>
2018-04-14 17:40:02 +0800

    add result.go

---
+++ common/result.go
@@ -0,0 +1,30 @@
+package common
+
+const (
+	// Success 成功
+	Success = iota
+	// Failed 失败
+	Failed
+	// InvalidAuthority 非法授权
+	InvalidAuthority
+	// NoExist 对象不存在
+	NoExist
+)
+
+// Result 处理结果
+// ErrorCode 错误码
+// Reason 错误信息
+type Result struct {
+	ErrorCode int    `json:"errorCode"`
+	Reason    string `json:"reason"`
+}
+
+// Success 成功
+func (result *Result) Success() bool {
+	return result.ErrorCode == Success
+}
+
+// Fail 失败
+func (result *Result) Fail() bool {
+	return result.ErrorCode != Success
+}
