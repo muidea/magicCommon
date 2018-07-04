a85cb8cee2e674f592d98c0b619f6090790aa562
muidea <rangh@foxmail.com>
2018-07-04 19:34:24 +0800

    update FileSummary

--- model/file.go
+++ model/file.go
@@ -2,7 +2,7 @@ package model
 
 // FileSummary 文件信息
 type FileSummary struct {
-	AccessToken string `json:"accessToken"`
+	FileToken   string `json:"fileToken"`
 	FileName    string `json:"fileName"`
 	FilePath    string `json:"filePath"`
 	UploadDate  string `json:"uploadDate"`
