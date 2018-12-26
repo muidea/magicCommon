05055550c10054bee3619c113c028f0b99f51966
muidea <rangh@foxmail.com>
2018-12-26 22:27:45 +0800

    update code

--- orm/util/const.go
+++ orm/util/const.go
@@ -2,8 +2,6 @@ package util
 
 import (
 	"fmt"
-	"reflect"
-	"time"
 )
 
 // Define the Type enum
@@ -25,7 +23,6 @@ const (
 	TypeDoubleField
 	TypeStructField
 	TypeSliceField
-	TypePtrField
 )
 
 // Field type
@@ -35,13 +32,13 @@ const (
 	TypeReferencePtrField
 )
 
-// IsSimpleField IsSimpleField
-func IsSimpleField(typeValue int) bool {
+// IsBaseTypeValue IsBaseTypeValue
+func IsBaseTypeValue(typeValue int) bool {
 	return typeValue < TypeStructField
 }
 
-// GetInitValue GetInitValue
-func GetInitValue(typeValue int) (ret interface{}) {
+// GetBaseTypeInitValue GetBaseTypeInitValue
+func GetBaseTypeInitValue(typeValue int) (ret interface{}) {
 	switch typeValue {
 	case TypeBooleanField,
 		TypeBitField, TypeSmallIntegerField, TypeIntegerField, TypeInteger32Field, TypeBigIntegerField:
@@ -60,109 +57,15 @@ func GetInitValue(typeValue int) (ret interface{}) {
 		val := 0.00
 		ret = &val
 		break
+	case TypeStructField:
+		val := 0
+		ret = &val
+	case TypeSliceField:
+		val := ""
+		ret = &val
 	default:
 		msg := fmt.Sprintf("no support fileType, %d", typeValue)
 		panic(msg)
 	}
-	return
-}
-
-// GetValue get value by special type
-func GetValue(typeValue int, val reflect.Value) (ret reflect.Value) {
-	switch typeValue {
-	case TypeBooleanField:
-		switch val.Type().Kind() {
-		case reflect.Int:
-			v := false
-			if val.Int() > 0 {
-				v = true
-			}
-			ret = reflect.ValueOf(v)
-		case reflect.Bool:
-			ret = val
-		default:
-			msg := fmt.Sprintf("unexception value, exception type:[%d], current value:[%v]", typeValue, val.Interface())
-			panic(msg)
-		}
-	case TypeDateTimeField:
-		switch val.Type().Kind() {
-		case reflect.String:
-			ts, err := time.ParseInLocation("2006-01-02 15:04:05", val.String(), time.Local)
-			if err != nil {
-				msg := fmt.Sprintf("unexception value, exception type:[%d], current value:[%v]", typeValue, val.Interface())
-				panic(msg)
-			}
-			ret = reflect.ValueOf(ts)
-		case reflect.Struct:
-			if val.Type().String() == "time.Time" {
-				ret = val
-			} else {
-				msg := fmt.Sprintf("unexception value, exception type:[%d], current value:[%v]", typeValue, val.Interface())
-				panic(msg)
-			}
-		default:
-			msg := fmt.Sprintf("unexception value, exception type:[%d], current value:[%v]", typeValue, val.Interface())
-			panic(msg)
-		}
-	case TypeBitField, TypeSmallIntegerField, TypeIntegerField, TypeInteger32Field, TypeBigIntegerField,
-		TypePositiveBitField, TypePositiveSmallIntegerField, TypePositiveIntegerField, TypePositiveInteger32Field, TypePositiveBigIntegerField:
-		switch val.Type().Kind() {
-		case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int:
-			if !ret.IsValid() {
-				ret = val
-			} else {
-				ret.SetInt(val.Int())
-			}
-		case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint:
-			if !ret.IsValid() {
-				ret = val
-			} else {
-				ret.SetUint(val.Uint())
-			}
-		default:
-			msg := fmt.Sprintf("unexception value, exception type:[%d], current value:[%v]", typeValue, val.Interface())
-			panic(msg)
-		}
-	case TypeFloatField, TypeDoubleField:
-		switch val.Type().Kind() {
-		case reflect.Float32, reflect.Float64:
-			if !ret.IsValid() {
-				ret = val
-			} else {
-				ret.SetFloat(val.Float())
-			}
-		default:
-			msg := fmt.Sprintf("unexception value, exception type:[%d], current value:[%v]", typeValue, val.Interface())
-			panic(msg)
-		}
-	case TypeStringField:
-		switch val.Type().Kind() {
-		case reflect.String:
-			if !ret.IsValid() {
-				ret = val
-			} else {
-				ret.SetString(val.String())
-			}
-		default:
-			msg := fmt.Sprintf("unexception value, exception type:[%d], current value:[%v]", typeValue, val.Interface())
-			panic(msg)
-		}
-	case TypeStructField:
-		switch val.Type().Kind() {
-		case reflect.Struct:
-			if !ret.IsValid() {
-				ret = val
-			} else {
-				ret.Set(val)
-			}
-		default:
-			msg := fmt.Sprintf("unexception value, exception type:[%d], current value:[%v]", typeValue, val.Interface())
-			panic(msg)
-		}
-	default:
-		msg := fmt.Sprintf("unexception value, exception type:[%d], current value:[%v]", typeValue, val.Interface())
-		panic(msg)
-	}
-
 	return
 }
