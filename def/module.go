23ce7ea275c400d41c809c56d682969ae79782fb
muidea <rangh@foxmail.com>
2019-10-08 13:49:24 +0800

    删除无用文件

--- def/module.go
+++
@@ -1,9 +0,0 @@
-package def
-
-import "github.com/muidea/magicCommon/model"
-
-// GetModuleListResult 获取ModuleList
-type GetModuleListResult struct {
-	Result
-	Module []model.ModuleDetailView `json:"module"`
-}
