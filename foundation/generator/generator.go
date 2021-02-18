7336546ea7f704a48dfed0fc3a0c80897822dc6f
muidea <rangh@foxmail.com>
2021-02-18 16:25:11 +0800

    update code

--- foundation/generator/generator.go
+++ foundation/generator/generator.go
@@ -3,6 +3,7 @@ package generator
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"time"
 )
 
@@ -11,8 +12,16 @@ type Generator interface {
 }
 
 /*
+prefix-{num}
+prefix-{123}
+prefix-{fixed(12):123}
+prefix-{fixed(12):num}
 prefix-{YYYYMMDDHHmmSS}-{num}
+prefix-{YYYYMMDDHHmmSS}-{123}
+prefix-{YYYYMMDDHHmmSS}-{fixed(12):123}
+prefix-{YYYYMMDDHHmmSS}-{fixed(12):num}
 */
+
 func New(mask string) (ret Generator, err error) {
 	maskFlag, maskErr := regexp.MatchString(maskPattern, mask)
 	if !maskFlag || maskErr != nil {
@@ -20,23 +29,49 @@ func New(mask string) (ret Generator, err error) {
 		return
 	}
 
-	return &genImpl{maskStr: mask, currentNum: 0}, nil
+	fixedWidth := "4"
+	numMask := numberReg.FindString(mask)
+	fixedStr := regexp.MustCompile("fixed\\([0-9]+\\)").FindString(numMask)
+	if fixedStr != "" {
+		fixedWidth = regexp.MustCompile("[0-9]+").FindString(fixedStr)
+	}
+
+	currentNum := 0
+	numStr := regexp.MustCompile("(num|[0-9]+)}$").FindString(numMask)
+	if numStr != "" {
+		numStr = regexp.MustCompile("(num|[0-9]+)").FindString(numStr)
+		iVal, iErr := strconv.Atoi(numStr)
+		if iErr == nil {
+			currentNum = iVal
+		}
+	}
+
+	return &genImpl{maskStr: mask, fixedWidth: fixedWidth, currentNum: currentNum}, nil
 }
 
-var maskPattern = "^[a-zA-Z]+[a-zA-Z0-9]*-|{YYYYMMDDHHmmSS}|-{num}$"
+var maskPattern = "^([a-zA-Z]+[a-zA-Z0-9]*-)?({YYYYMMDDHHmmSS}-)?{(fixed\\([0-9]+\\)\\:)?(num|[0-9]+)}$"
 var prefixReg = regexp.MustCompile("^[a-zA-Z]+[a-zA-Z0-9]*")
 var dateTimeReg = regexp.MustCompile("{YYYYMMDDHHmmSS}")
-var numReg = regexp.MustCompile("{num}$")
+var numberReg = regexp.MustCompile("{(fixed\\([0-9]+\\)\\:)?(num|[0-9]+)}$")
 
 type genImpl struct {
 	maskStr    string
+	fixedWidth string
 	currentNum int
 }
 
 func (s *genImpl) GenCode() string {
-	s.currentNum++
-	datTime := time.Now().Local().Format("20060102150405")
-	result := dateTimeReg.ReplaceAllString(s.maskStr, datTime)
-	result = numReg.ReplaceAllString(result, fmt.Sprintf("%04d", s.currentNum))
+	result := dateTimeReg.ReplaceAllString(s.maskStr, s.genDateTime())
+	result = numberReg.ReplaceAllString(result, s.genNum())
 	return result
+}
+
+func (s *genImpl) genDateTime() string {
+	return time.Now().Local().Format("20060102150405")
+}
+
+func (s *genImpl) genNum() string {
+	s.currentNum++
+	mask := fmt.Sprintf("%%0%sd", s.fixedWidth)
+	return fmt.Sprintf(mask, s.currentNum)
 }
--- foundation/generator/generator_test.go
+++ foundation/generator/generator_test.go
@@ -8,14 +8,14 @@ import (
 
 func TestGenImpl_GenCode(t *testing.T) {
 	datTime := time.Now().Local().Format("20060102150405")
-	generator1, generatorErr := New("prefix-{YYYYMMDDHHmmSS}-{num}")
+	generator, generatorErr := New("prefix-{YYYYMMDDHHmmSS}-{num}")
 	if generatorErr != nil {
 		t.Errorf("illgel gengerator")
 		return
 	}
 
 	expect := fmt.Sprintf("prefix-%s-%04d", datTime, 1)
-	result := generator1.GenCode()
+	result := generator.GenCode()
 	if expect != result {
 		t.Errorf("genCode failed, expect:%s, result:%s", expect, result)
 		return
@@ -24,6 +24,32 @@ func TestGenImpl_GenCode(t *testing.T) {
 	_, generatorErr = New("{YYYYMMDDHHmmSS}-{num}")
 	if generatorErr != nil {
 		t.Errorf("illgel gengerator")
+		return
+	}
+
+	generator, generatorErr = New("prefix-{YYYYMMDDHHmmSS}-{fixed(5):123}")
+	if generatorErr != nil {
+		t.Errorf("illgel gengerator")
+		return
+	}
+
+	expect = fmt.Sprintf("prefix-%s-%05d", datTime, 124)
+	result = generator.GenCode()
+	if expect != result {
+		t.Errorf("genCode failed, expect:%s, result:%s", expect, result)
+		return
+	}
+
+	generator, generatorErr = New("prefix-{fixed(5):123}")
+	if generatorErr != nil {
+		t.Errorf("illgel gengerator")
+		return
+	}
+
+	expect = fmt.Sprintf("prefix-%05d", 124)
+	result = generator.GenCode()
+	if expect != result {
+		t.Errorf("genCode failed, expect:%s, result:%s", expect, result)
 		return
 	}
 }
