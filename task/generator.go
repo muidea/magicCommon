ac102f98b2aaa90eba44338df03e8df6c511157a
muidea <rangh@foxmail.com>
2021-02-18 13:34:01 +0800

    update code

---
+++ task/generator.go
@@ -0,0 +1,17 @@
+package task
+
+import "github.com/muidea/magicCommon/foundation/generator"
+
+type GeneratorTask struct {
+	generator generator.Generator
+
+	genCode string
+}
+
+func (s *GeneratorTask) Run() {
+	s.genCode = s.generator.GenCode()
+}
+
+func (s *GeneratorTask) Result() string {
+	return s.genCode
+}
