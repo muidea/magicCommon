68054d6bf5801c600c8d3c274e9a08a07be959ff
muidea <rangh@foxmail.com>
2021-05-10 21:47:41 +0800

    update code

--- def/filter.go
+++ def/filter.go
@@ -1,6 +1,7 @@
 package def
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 
@@ -15,7 +16,7 @@ type Filter struct {
 
 // NewFilter new filter
 func NewFilter() *Filter {
-	return &Filter{}
+	return &Filter{PageFilter: nil, ContentFilter: &ContentFilter{}}
 }
 
 // Decode 内容过滤器
@@ -45,7 +46,7 @@ func (s *Filter) Encode(vals url.Values) url.Values {
 	return vals
 }
 
-func (s *Filter) Get(key string) (val string, ok bool) {
+func (s *Filter) Get(key string) (val interface{}, ok bool) {
 	if s.ContentFilter != nil {
 		val, ok = s.ContentFilter.Items[key]
 		return
@@ -54,14 +55,20 @@ func (s *Filter) Get(key string) (val string, ok bool) {
 	return
 }
 
+func (s *Filter) Set(key string, value interface{}) {
+	if s.ContentFilter != nil {
+		s.ContentFilter.Items[key] = value
+	}
+}
+
 // ContentFilter contentFilter
 type ContentFilter struct {
-	Items map[string]string
+	Items map[string]interface{}
 }
 
 // Decode 解析内容过滤值
 func (s *ContentFilter) Decode(request *http.Request) bool {
-	s.Items = map[string]string{}
+	s.Items = map[string]interface{}{}
 	vals := request.URL.Query()
 	for k, v := range vals {
 		s.Items[k] = v[0]
@@ -73,8 +80,8 @@ func (s *ContentFilter) Decode(request *http.Request) bool {
 // Encode ContentFilter
 func (s *ContentFilter) Encode(vals url.Values) url.Values {
 	for k, v := range s.Items {
-		if v != "" {
-			vals.Set(k, v)
+		if v != nil {
+			vals.Set(k, fmt.Sprintf("%v", v))
 		}
 	}
 
