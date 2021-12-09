ea3622b9113850b281e6d7d2ef9c728777d81de3
muidea <rangh@foxmail.com>
2021-12-09 09:08:10 +0800

    update code

--- foundation/util/configurate.go
+++ foundation/util/configurate.go
@@ -8,7 +8,7 @@ import (
 )
 
 func LoadConfig(filePath string, ptr interface{}) (err error) {
-	fileHandle, fileErr := os.OpenFile(filePath, os.O_RDONLY, os.ModeType)
+	fileHandle, fileErr := os.OpenFile(filePath, os.O_RDONLY, os.ModePerm)
 	if fileErr != nil {
 		err = fileErr
 		return
@@ -36,7 +36,7 @@ func SaveConfig(filePath string, ptr interface{}) (err error) {
 		return
 	}
 
-	fileHandle, fileErr := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModeType)
+	fileHandle, fileErr := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
 	if fileErr != nil {
 		err = fileErr
 		return
