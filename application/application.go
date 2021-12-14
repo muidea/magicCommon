7f09c4d49dc18dff0dc76bccc4b94b98bafa2d5f
muidea <rangh@foxmail.com>
2021-12-14 21:46:34 +0800

    update code

---
+++ application/application.go
@@ -0,0 +1,110 @@
+package application
+
+import (
+	"log"
+	"sync"
+
+	"github.com/muidea/magicCommon/event"
+	"github.com/muidea/magicCommon/module"
+	"github.com/muidea/magicCommon/task"
+)
+
+type Application interface {
+	EventHub() event.Hub
+	BackgroundRoutine() task.BackgroundRoutine
+	BindService(service module.Service)
+	UnbindService(service module.Service)
+	Startup()
+	Run()
+	Teardown()
+}
+
+var application Application
+var applicationOnce sync.Once
+
+func GetApp() Application {
+	applicationOnce.Do(func() {
+		application = &appImpl{
+			backgroundRoutine: task.NewBackgroundRoutine(),
+			eventHub:          event.NewHub(),
+		}
+	})
+
+	return application
+}
+
+type appImpl struct {
+	backgroundRoutine task.BackgroundRoutine
+	eventHub          event.Hub
+	name2Service      sync.Map
+}
+
+func (s *appImpl) EventHub() event.Hub {
+	return s.eventHub
+}
+
+func (s *appImpl) BackgroundRoutine() task.BackgroundRoutine {
+	return s.backgroundRoutine
+}
+
+func (s *appImpl) BindService(service module.Service) {
+	_, ok := s.name2Service.Load(service.Name())
+	if ok {
+		log.Fatalf("duplicate service, name:%s", service.Name())
+		return
+	}
+
+	s.name2Service.Store(service.Name(), service)
+}
+
+func (s *appImpl) UnbindService(service module.Service) {
+	s.name2Service.Delete(service.Name())
+}
+
+func (s *appImpl) Startup() {
+	var wg sync.WaitGroup
+	s.name2Service.Range(func(key, value interface{}) bool {
+		service := value.(module.Service)
+		wg.Add(1)
+		go func() {
+			service.Startup()
+			wg.Done()
+		}()
+
+		return true
+	})
+
+	wg.Wait()
+}
+
+func (s *appImpl) Run() {
+	var wg sync.WaitGroup
+	s.name2Service.Range(func(key, value interface{}) bool {
+		service := value.(module.Service)
+		wg.Add(1)
+		go func() {
+			service.Run()
+			wg.Done()
+		}()
+
+		return true
+	})
+
+	wg.Wait()
+}
+
+func (s *appImpl) Teardown() {
+	var wg sync.WaitGroup
+	s.name2Service.Range(func(key, value interface{}) bool {
+		service := value.(module.Service)
+		wg.Add(1)
+		go func() {
+			service.Teardown()
+			wg.Done()
+		}()
+
+		return true
+	})
+
+	wg.Wait()
+}
---
+++ module/module.go
@@ -0,0 +1,35 @@
+package module
+
+type Module interface {
+	ID() string
+	Startup()
+	Shutdown()
+}
+
+type Service interface {
+	Name() string
+	Startup()
+	Run()
+	Teardown()
+}
+
+var name2ModuleList map[string][]Module
+
+func Register(serviceName string, module Module) {
+	list, ok := name2ModuleList[serviceName]
+	if !ok {
+		list = []Module{}
+	}
+
+	list = append(list, module)
+	name2ModuleList[serviceName] = list
+}
+
+func GetModules(serviceName string) []Module {
+	list, ok := name2ModuleList[serviceName]
+	if ok {
+		return list
+	}
+
+	return []Module{}
+}
