6a835e2c961eefbbf49777fa882c6678867a9dfe
muidea <rangh@foxmail.com>
2021-12-19 21:39:08 +0800

    update code

--- module/module.go
+++ module/module.go
@@ -1,6 +1,7 @@
 package module
 
 import (
+	"fmt"
 	"github.com/muidea/magicCommon/event"
 	"github.com/muidea/magicCommon/task"
 	"reflect"
@@ -37,4 +38,38 @@ func validModule(ptr interface{}) {
 	if !idOK || !setupOK || !teardownOK {
 		panic("invalid module")
 	}
+}
+
+func Setup(endpointName string, eventHub event.Hub, backgroundRoutine task.BackgroundRoutine, module interface{}) (ret interface{}, err error) {
+	vVal := reflect.ValueOf(module)
+	vVal = vVal.Elem()
+
+	setupFun := vVal.MethodByName("Setup")
+	if setupFun.IsNil() {
+		err = fmt.Errorf("illegal module")
+		return
+	}
+
+	param := make([]reflect.Value, 3)
+	param[0] = reflect.ValueOf(endpointName)
+	param[1] = reflect.ValueOf(eventHub)
+	param[2] = reflect.ValueOf(backgroundRoutine)
+
+	setupFun.Call(param)
+	return
+}
+
+func Teardown(module interface{}) (ret interface{}, err error) {
+	vVal := reflect.ValueOf(module)
+	vVal = vVal.Elem()
+
+	teardownFun := vVal.MethodByName("Teardown")
+	if teardownFun.IsNil() {
+		err = fmt.Errorf("illegal module")
+		return
+	}
+
+	param := make([]reflect.Value, 0)
+	teardownFun.Call(param)
+	return
 }
