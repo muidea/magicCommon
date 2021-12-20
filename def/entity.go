4d1c3ea3c7d77f2aad0f82f658be1bef0a315850
muidea <rangh@foxmail.com>
2021-12-20 18:04:56 +0800

    update code

--- def/entity.go
+++ def/entity.go
@@ -1,7 +1,38 @@
 package def
 
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
 type Entity struct {
 	EName string `json:"ename"`
 	EID   int    `json:"eid"`
 	EType string `json:"etype"`
+}
+
+// Decode decode entityPtr
+func (s *Entity) Decode(req *http.Request) (err error) {
+	str := req.URL.Query().Get("entityID")
+	if str != "" {
+		s.EID, err = strconv.Atoi(str)
+		if err != nil {
+			return
+		}
+	} else {
+		err = fmt.Errorf("illegal entity info")
+	}
+	s.EName = req.URL.Query().Get("entityName")
+	s.EType = req.URL.Query().Get("entityType")
+	return
+}
+
+// Encode encode entityPtr
+func (s *Entity) Encode(vals url.Values) url.Values {
+	vals.Set("entityID", fmt.Sprintf("%d", s.EID))
+	vals.Set("entityName", s.EName)
+	vals.Set("entityType", s.EType)
+	return vals
 }
