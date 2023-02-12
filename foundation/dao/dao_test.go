e227dbdc83bc3e5cea84ff280be583f3b0f08fe7
muidea <rangh@foxmail.com>
2023-02-12 20:11:28 +0800

    update code

--- foundation/dao/dao_test.go
+++ foundation/dao/dao_test.go
@@ -2,58 +2,93 @@ package dao_test
 
 import (
 	"fmt"
-	"log"
 	"testing"
 
 	"github.com/muidea/magicCommon/foundation/dao"
 )
 
 type User struct {
-	id       int
-	name     string
-	password string
-	catalog  int
+	id      int
+	address string
 }
 
-func testFun(t *testing.T) {
-	dao, err := dao.Fetch("magiccenter", "magiccenter", "localhost:3306", "magiccenter_db")
+const user = "root"
+const password = "rootkit"
+const svrAddress = "localhost:3306"
+const dbName = "testDB"
+
+func TestDatabase(t *testing.T) {
+	dao, err := dao.Fetch(user, password, svrAddress, "")
 	if err != nil {
 		t.Errorf("Fetch dao failed, err:%s", err.Error())
 	}
 	defer dao.Release()
 
-	insertSql := fmt.Sprintf("%s", "insert into magiccenter_db.user value(1,'test3','test3','test11','aaa',1)")
+	defer func() {
+		dropDbSql := fmt.Sprintf("drop database if exists %s", dbName)
+		dao.Execute(dropDbSql)
+	}()
+
+	createDbSql := fmt.Sprintf("create database if not exists %s", dbName)
+	num := dao.Execute(createDbSql)
+	if num != 1 {
+		t.Errorf("create database failed")
+	}
+}
+
+func initFunc(dao dao.Dao) {
+	dbSql := fmt.Sprintf("create database if not exists %s", dbName)
+	dao.Execute(dbSql)
+
+	useSql := fmt.Sprintf("use %s", dbName)
+	dao.Execute(useSql)
+
+	tableSql :=
+		`
+CREATE TABLE IF NOT EXISTS user (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  address text(125),
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+`
+	dao.Execute(tableSql)
+}
+
+func TestInsert(t *testing.T) {
+	dao, err := dao.Fetch(user, password, svrAddress, "")
+	if err != nil {
+		t.Errorf("Fetch dao failed, err:%s", err.Error())
+	}
+	defer dao.Release()
+
+	initFunc(dao)
+
+	insertSql := fmt.Sprintf("%s", "insert into user (address) values(\"abc\")")
 	num := dao.Execute(insertSql)
-	if num == 1 {
+	if num != 1 {
 		t.Errorf("Insert data failed")
 	}
 
-	querySql := "select * from magiccenter_db.user where id=1"
+	querySql := "select * from user where id=1"
 	dao.Query(querySql)
 }
 
-func TestInser(t *testing.T) {
-	testFun(t)
-
-	forever := make(chan bool)
-	log.Printf(" [*] To exit press CTRL+C")
-	<-forever
-}
-
 func TestQuery(t *testing.T) {
-	dao, err := dao.Fetch("magiccenter", "magiccenter", "localhost:3306", "magiccenter_db")
+	dao, err := dao.Fetch(user, password, svrAddress, "")
 	if err != nil {
 		t.Errorf("Fetch dao failed, err:%s", err.Error())
 	}
 	defer dao.Release()
 
-	selectSql := fmt.Sprint("select id,name,password,type from magiccenter_db.user")
+	initFunc(dao)
+
+	selectSql := fmt.Sprint("select id,address from user")
 
 	dao.Query(selectSql)
 
 	for dao.Next() {
 		user := User{}
 
-		dao.GetField(&user.id, &user.name, &user.password, &user.catalog)
+		dao.GetField(&user.id, &user.address)
 	}
 }
