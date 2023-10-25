0574e9abea17272320bbc7d3480deb1230f3dcbf
muidea <rangh@foxmail.com>
2023-10-25 14:25:09 +0800

    update code

--- def/result.go
+++ def/result.go
@@ -8,20 +8,22 @@ import (
 type ErrorCode int
 
 const (
-	// Success 成功
-	Success = iota
-	// Failed 失败
-	Failed
-	// IllegalParam 非法参数
-	IllegalParam
-	// InvalidAuthority 非法授权
-	InvalidAuthority
+	// Succeeded 成功
+	Succeeded = iota
+	// Warned 警告
+	Warned = 200000
 	// NoExist 对象不存在
-	NoExist
+	NoExist = 200001
+	// Failed 失败
+	Failed = 500000
+	// IllegalParam 非法参数
+	IllegalParam = 500001
+	// InvalidAuthority 非法授权
+	InvalidAuthority = 500002
 	// Redirect 对象转移
-	Redirect
+	Redirect = 500003
 	// UnExpected 意外错误
-	UnExpected
+	UnExpected = 500004
 )
 
 // Result 处理结果
@@ -45,26 +47,42 @@ type CommonSliceResult struct {
 
 // Success 成功
 func (s *Result) Success() bool {
-	return s.ErrorCode == Success
+	return s.ErrorCode == Succeeded
+}
+
+func (s *Result) Warn() bool {
+	return s.ErrorCode >= Warned && s.ErrorCode < Failed
 }
 
 // Fail 失败
 func (s *Result) Fail() bool {
-	return s.ErrorCode != Success
+	return s.ErrorCode >= Failed
 }
 
 func (s *Result) Error() error {
-	if s.ErrorCode == Success {
+	if s.ErrorCode == Succeeded {
 		return nil
 	}
 
-	return fmt.Errorf("errorCode:%v, reason:%v", s.ErrorCode, s.Reason)
+	if s.Reason != "" {
+		return fmt.Errorf("errorCode:%v, reason:%v", s.ErrorCode, s.Reason)
+	}
+
+	return fmt.Errorf("errorCode:%v", s.ErrorCode)
 }
 
-func GetError(errCode ErrorCode, reason string) error {
-	if errCode == Success {
-		return nil
+func GetError(errCode ErrorCode, reason string) *Result {
+	if errCode < Failed {
+		errCode = Failed
 	}
 
-	return fmt.Errorf("errorCode:%v, reason:%v", errCode, reason)
+	return &Result{ErrorCode: errCode, Reason: reason}
+}
+
+func NewWarn(errCode ErrorCode, reason string) *Result {
+	if errCode < Warned || errCode >= Failed {
+		errCode = Warned
+	}
+
+	return &Result{ErrorCode: errCode, Reason: reason}
 }
