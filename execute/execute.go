4ee37f935663bc90506df1a42ef3ac4f99c140bf
muidea <rangh@foxmail.com>
2023-11-20 19:00:27 +0800

    feat: 添加主动异常捕获，提升运行健壮性

--- execute/execute.go
+++ execute/execute.go
@@ -1,5 +1,10 @@
 package execute
 
+import (
+	"github.com/muidea/magicCommon/foundation/log"
+	"github.com/muidea/magicCommon/foundation/util"
+)
+
 type Execute struct {
 	capacityQueue chan bool
 }
@@ -14,10 +19,22 @@ func NewExecute(capacitySize int) Execute {
 	}
 }
 
+func (s *Execute) Lock() { /* for noCopy */ }
+
+func (s *Execute) Unlock() { /* for noCopy */ }
+
 func (s *Execute) Run(funcPtr func()) {
 	s.capacityQueue <- true
 	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				stackInfo := util.GetStack(3)
+				log.Errorf("PANIC: %v\n%s", err, stackInfo)
+			}
+
+			<-s.capacityQueue
+		}()
+
 		funcPtr()
-		<-s.capacityQueue
 	}()
 }
