45d7e61acd9346fbb2ea9583d30e026620e25c49
muidea <rangh@foxmail.com>
2024-03-02 12:57:00 +0800

    fix: 修正isEmptyDir检查判断错误

--- foundation/path/path_test.go
+++ foundation/path/path_test.go
@@ -24,6 +24,8 @@ func TestIsDirEmpty(t *testing.T) {
 
 	emptyDir := path.Join(curWD, "emptydir")
 	t.Logf("emptyDir:%s", emptyDir)
+	os.Mkdir(emptyDir, os.ModePerm)
+	defer os.Remove(emptyDir)
 	isEmpty, isErr = IsDirEmpty(emptyDir)
 	if isErr != nil {
 		t.Errorf("check is dir empty failed, error:%s", isErr.Error())
