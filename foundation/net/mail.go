7d7f83fc45e838fc8cca4060ef605aecb01affb2
muidea <rangh@foxmail.com>
2024-03-07 14:32:56 +0800

    feat: 1、支持sendMail支持是否skipTls，2、支持发送附件

--- foundation/net/mail.go
+++ foundation/net/mail.go
@@ -1,30 +1,48 @@
 package net
 
 import (
-	"net/smtp"
-	"strings"
+	"crypto/tls"
+	"gopkg.in/gomail.v2"
+)
+
+const (
+	Html = "html"
+	Text = "text"
 )
 
 // SendMail 发送邮件
 // user : example@example.com login smtp server user
 // password: xxxxx login smtp server password
-// host: smtp.example.com:port   smtp.163.com:25
-// to: example@example.com;example1@163.com;example2@sina.com.cn;...
+// emailServer: smtp.example.com   smtp.163.com
+// emailPort: 25
+// sendTo, ccTo: example@example.com;example1@163.com;example2@sina.com.cn;...
 // subject:The subject of mail
-// body: The content of mail
-// mailtyoe: mail type html or text
-func SendMail(user, password, host string, sendTo []string, subject, content string, attachment []string, mailtype string) error {
-	hp := strings.Split(host, ":")
-	auth := smtp.PlainAuth("", user, password, hp[0])
-	var contentType string
-	if mailtype == "html" {
-		contentType = "Content-Type: text/" + mailtype + "; charset=UTF-8"
+// content: The content of mail
+// attachment: 附件
+// mimeType: mail type html or text
+// skipTls: 是否忽略Tls
+func SendMail(user, password, emailServer string, emailPort int, sendTo, ccTo []string, subject, content string, attachment []string, mimeType string, skipTls bool) error {
+	goMailMsg := gomail.NewMessage()
+	goMailMsg.SetHeader("From", user)
+	goMailMsg.SetHeader("To", sendTo...)
+	if len(ccTo) > 0 {
+		goMailMsg.SetHeader("Cc", ccTo...)
+	}
+	goMailMsg.SetHeader("Subject", subject)
+	if mimeType == Html {
+		goMailMsg.SetBody("text/html", content)
 	} else {
-		contentType = "Content-Type: text/plain" + "; charset=UTF-8"
+		goMailMsg.SetBody("text/plain", content)
 	}
 
-	mailList := strings.Join(sendTo, ";")
-	msg := []byte("To: " + mailList + "\r\nFrom: " + user + "<" + user + ">\r\nSubject: " + subject + "\r\n" + contentType + "\r\n\r\n" + content)
-	err := smtp.SendMail(host, auth, user, sendTo, msg)
-	return err
+	for _, val := range attachment {
+		goMailMsg.Attach(val)
+	}
+
+	goMailDialer := gomail.NewDialer(emailServer, emailPort, user, password)
+	if skipTls {
+		goMailDialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	}
+
+	return goMailDialer.DialAndSend(goMailMsg)
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/google/uuid v1.3.0
 	golang.org/x/sync v0.4.0
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
@@ -18,4 +19,5 @@ require (
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 )
