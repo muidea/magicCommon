53a783bc332340f373ac01fe2d7daca134eff733
muidea <rangh@foxmail.com>
2024-08-14 17:06:31 +0800

    feat(): add signalGard

---
+++ foundation/signal/signal.go
@@ -0,0 +1,67 @@
+package signal
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/muidea/magicCommon/foundation/log"
+)
+
+type Gard struct {
+	signalChanMap sync.Map
+}
+
+func (s *Gard) PutSignal(id int) (err error) {
+	_, ok := s.signalChanMap.Load(id)
+	if ok {
+		msg := fmt.Sprintf("duplicate signal %d", id)
+		err = fmt.Errorf(msg)
+		log.Errorf(msg)
+		return
+	}
+
+	signalChan := make(chan bool, 1)
+	s.signalChanMap.Store(id, signalChan)
+	return
+}
+
+func (s *Gard) WaitSignal(id, timeOut int) (err error) {
+	signalChan, signalOK := s.signalChanMap.Load(id)
+	if !signalOK {
+		msg := fmt.Sprintf("can't find signal %d", id)
+		err = fmt.Errorf(msg)
+		log.Errorf(msg)
+		return
+	}
+
+	if timeOut < 0 {
+		timeOut = 60 * 60
+	}
+	timeOutVal := time.Duration(timeOut) * time.Second
+	select {
+	case <-signalChan.(chan bool):
+	case <-time.After(timeOutVal):
+		msg := fmt.Sprintf("wait signal %d timeout", id)
+		err = fmt.Errorf(msg)
+		log.Warnf(msg)
+		signalChan.(chan bool) <- true
+	}
+
+	close(signalChan.(chan bool))
+	s.signalChanMap.Delete(id)
+	return
+}
+
+func (s *Gard) TriggerSignal(id int) (err error) {
+	signalChan, signalOK := s.signalChanMap.Load(id)
+	if !signalOK {
+		msg := fmt.Sprintf("can't find signal %d", id)
+		err = fmt.Errorf(msg)
+		log.Errorf(msg)
+		return
+	}
+
+	signalChan.(chan bool) <- true
+	return
+}
